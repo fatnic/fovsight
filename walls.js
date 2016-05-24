5c9de204facd4df63bc4089b33fe8c71eb84a653
Dave Nicholson <fatnic388@gmail.com>
2016-05-24 06:46:24 +0100

    moved grid function to Wall object

--- walls.js
+++ walls.js
@@ -1,18 +1,18 @@
 var Walls = {
     segments: [],
     boxes:[],
+    grid: 16,
     add: function(x,y,w,h,solid=true){
         this.segments.push({a:{x:x,y:y}, b:{x:x+w,y:y}});
         this.segments.push({a:{x:x+w,y:y}, b:{x:x+w,y:y+h}});
         this.segments.push({a:{x:x+w,y:y+h}, b:{x:x,y:y+h}});
         this.segments.push({a:{x:x,y:y+h}, b:{x:x,y:y}});
         if(solid) this.boxes.push({x:x,y:y,w:w,h:h});
+    },
+    addByGrid: function(x,y,w,h){
+        this.add(x*this.grid, y*this.grid, w*this.grid, h*this.grid);
     }
 };
-
-function gridWall(x,y,w,h,g=16){
-    Walls.add(x*g,y*g,w*g,h*g);
-}
 
 // Outer wall
 Walls.add(0, 0, canvas.width, canvas.height, false);
@@ -27,8 +27,8 @@ Walls.add(400,50,100,50);
 Walls.add(750,480,150,50);
 
 // Room walls
-gridWall(4, 20, 12, 1);
-gridWall(4, 30, 12, 1);
-gridWall(4, 21, 1, 9);
-gridWall(15, 21, 1, 3);
-gridWall(15, 27, 1, 3);
+Walls.addByGrid(4, 20, 12, 1);
+Walls.addByGrid(4, 30, 12, 1);
+Walls.addByGrid(4, 21, 1, 9);
+Walls.addByGrid(15, 21, 1, 3);
+Walls.addByGrid(15, 27, 1, 3);
